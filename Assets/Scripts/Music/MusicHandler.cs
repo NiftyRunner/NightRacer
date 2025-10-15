f57d20cafdfd51a69f0fc79a507374d03a6e4465
NiftyRunner <parthverma103@gmail.com>
2025-10-15 19:26:48 +0530

    Added music

---
+++ Assets/Scripts/Music/MusicHandler.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class MusicHandler : MonoBehaviour
+{
+    [SerializeField] AudioSource playerSource;
+    [SerializeField] AudioSource bgSource;
+    [SerializeField] AudioClip mainClip;
+    [SerializeField] AudioClip endClip;
+
+    private void OnEnable()
+    {
+        CollisionHandlerNew.OnPlayerCollision += CollisionHandlerNew_OnPlayerCollision;
+    }
+
+    private void OnDisable()
+    {
+        CollisionHandlerNew.OnPlayerCollision -= CollisionHandlerNew_OnPlayerCollision;
+    }
+
+    private void CollisionHandlerNew_OnPlayerCollision()
+    {
+        playerSource.Stop();
+        bgSource.Stop();
+        bgSource.clip = endClip;
+        bgSource.Play();
+    }
+
+    void Start()
+    {
+        playerSource.Play();
+        bgSource.clip = mainClip;
+        bgSource.Play();
+    }
+
+}
